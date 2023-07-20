316a176aad7d6b7ea2be74660256a2fb2fd972ce
unknown <daria.lantyugova@mail.ru>
2023-07-21 01:11:53 +0300

    wordlist table 2nd version

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,5 +1,9 @@
 import words from '../../data/words.json'
 import st from './table.module.scss';
+import edit from '../../assets/icons/edit.png';
+import del from '../../assets/icons/delete.png';
+import save from '../../assets/icons/save.png';
+import cancel from '../../assets/icons/cancel.png';
 
 
 
@@ -7,17 +11,25 @@ function Table() {
   return (
 <div className={st.wordlist}>
 <h1 className={st.wordlist__title} >List of words</h1>
+<div className={st.wordlist__optionButtonsContainer}>
+<button className={st.wordlist__optionButton}>Add a new word</button>
+<button className={st.wordlist__optionButton}>Study words</button>
+</div>
 <table className={st.wordlist__table}>
 <thead>
 <tr>
 <th>Word</th>
 <th>Transcription</th>
 <th>Translation</th>
+<th>Options</th>
 </tr>
 <tr className={st.wordlist__inputRow}>
     <td><input className={st.wordlist__input} type="text" placeholder="Word"></input></td>
     <td><input className={st.wordlist__input}  type="text" placeholder="Transcription"></input></td>
     <td><input className={st.wordlist__input}  type="text" placeholder="Translation"></input></td>
+    <td>
+    <img src={save} alt="save" className={st.wordlist__actionIcon} title="Save"/>
+    <img src={cancel} alt="cancel"className={st.wordlist__actionIcon} title="Cancel"/></td>
 </tr>
 </thead>
 <tbody> { words.map((item, index) => (
@@ -25,6 +37,9 @@ function Table() {
   <td>{item.english}</td>
   <td>{item.transcription}</td>
   <td>{item.russian}</td>
+  <td>
+    <img src={edit} alt="edit" className={st.wordlist__actionIcon} title="Edit"/>
+    <img src={del} alt="delete"className={st.wordlist__actionIcon} title="Delete"/></td>
 </tr>
 ))}
 

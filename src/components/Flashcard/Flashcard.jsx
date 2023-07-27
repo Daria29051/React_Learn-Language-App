37df65c8343e6d80c24a4f21f9b85a7bf6dd64f4
unknown <daria.lantyugova@mail.ru>
2023-07-27 14:18:35 +0300

    flashcard 2nd version

--- src/components/Flashcard/Flashcard.jsx
+++ src/components/Flashcard/Flashcard.jsx
@@ -1,38 +1,71 @@
-import React from 'react';
-import st from './flashcard.module.scss';
-import flashcards from '../../data/flashcard.json';
-
+import React from "react";
+import st from "./flashcard.module.scss";
+import flashcards from "../../data/flashcard.json";
 
 class Flashcard extends React.Component {
   constructor(props) {
-    super(props)
-  
+    super(props);
+
     this.state = {
-       pressed: false,
-    }
+      pressed: false,
+    };
   }
 
-  handleClick = () =>{
-    this.setState({
-      pressed: !this.state.pressed
-    });
+  handleChange = (index) => {
+   this.setState((prevState) => {
+    const updatedSelection = Array.from(prevState);
+    updatedSelection[index] = !updatedSelection[index];
+    console.log(updatedSelection);
+    return {pressed : updatedSelection[index]};
+   })
+  };
+
+  render() {
+    if (this.state.pressed) {
+      return (
+        <>
+          <div className={st.block}>
+            <h1 className={st.title}>Flashcards</h1>
+            <div className={st.container}>
+              {flashcards.map((item, index) => (
+                <div className={st.flashcard} key={index}>
+                  <h2 className={st.flashcard__word}>{item.english}</h2>
+                  <p className={st.flashcard__transcription}>
+                    {item.transcription}
+                  </p>
+                  <p className={st.flashcard__translation}>{item.russian}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <div className={st.block}>
+            <h1 className={st.title}>Flashcards</h1>
+            <div className={st.container}>
+              {flashcards.map((item, index) => (
+                <div className={st.flashcard} key={index}>
+                  <h2 className={st.flashcard__word}>{item.english}</h2>
+                  <p className={st.flashcard__transcription}>
+                    {item.transcription}
+                  </p>
+                  <button
+                    className={st.flashcard__button}
+                    onClick={() => this.handleChange(index)}
+                  >
+                    Проверить
+                  </button>
+                </div>
+              ))}
+            </div>
+          </div>
+        </>
+      );
+    }
   }
-render() {
-  return (<>
-  <div className={st.block}>
-  <h1 className={st.title}>Flashcards</h1>
-    <div  className={st.container} >
-    {flashcards.map((item, index) => (
-      <div className={st.flashcard} key={index}>
-<h2 className={st.flashcard__word}>{item.english}</h2>
-<p className={st.flashcard__transcription}>{item.transcription}</p>
-<button className={st.flashcard__button} onClick = {this.handleClick}>Проверить</button>
-</div>
-    ))}
-  </div>
-  </div>
-  </>
-  );
-}}
+}
 
 export default Flashcard;

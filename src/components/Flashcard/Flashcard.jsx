ec6c9df46da1740fd72eca04caf4eb3a6d8dc31c
unknown <daria.lantyugova@mail.ru>
2023-08-01 14:37:45 +0300

    handleChange function corrected in Flashcard.jsx

--- src/components/Flashcard/Flashcard.jsx
+++ src/components/Flashcard/Flashcard.jsx
@@ -13,58 +13,42 @@ class Flashcard extends React.Component {
 
   handleChange = (index) => {
     this.setState((prevState) => {
-      const updatedSelection = Array.from(prevState);
+      const updatedSelection = Array.from(prevState.pressed);
+      console.log(updatedSelection);
       updatedSelection[index] = !updatedSelection[index];
       console.log(updatedSelection);
-      return { pressed: updatedSelection[index] };
+      return { pressed: updatedSelection };
     });
   };
 
   render() {
-    if (this.state.pressed) {
-      return (
-        <>
-          <div className={st.block}>
-            <h1 className={st.title}>Flashcards</h1>
-            <div className={st.container}>
-              {flashcards.map((item, index) => (
-                <div className={st.flashcard} key={index}>
-                  <h2 className={st.flashcard__word}>{item.english}</h2>
-                  <p className={st.flashcard__transcription}>
-                    {item.transcription}
-                  </p>
+    return (
+      <>
+        <div className={st.block}>
+          <h1 className={st.title}>Flashcards</h1>
+          <div className={st.container}>
+            {flashcards.map((item, index) => (
+              <div className={st.flashcard} key={index}>
+                <h2 className={st.flashcard__word}>{item.english}</h2>
+                <p className={st.flashcard__transcription}>
+                  {item.transcription}
+                </p>
+                {this.state.pressed[index] ? (
                   <p className={st.flashcard__translation}>{item.russian}</p>
-                </div>
-              ))}
-            </div>
-          </div>
-        </>
-      );
-    } else {
-      return (
-        <>
-          <div className={st.block}>
-            <h1 className={st.title}>Flashcards</h1>
-            <div className={st.container}>
-              {flashcards.map((item, index) => (
-                <div className={st.flashcard} key={index}>
-                  <h2 className={st.flashcard__word}>{item.english}</h2>
-                  <p className={st.flashcard__transcription}>
-                    {item.transcription}
-                  </p>
+                ) : (
                   <button
                     className={st.flashcard__button}
                     onClick={() => this.handleChange(index)}
                   >
                     Проверить
                   </button>
-                </div>
-              ))}
-            </div>
+                )}
+              </div>
+            ))}
           </div>
-        </>
-      );
-    }
+        </div>
+      </>
+    );
   }
 }
 

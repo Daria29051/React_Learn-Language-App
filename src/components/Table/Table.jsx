ca64ab6e9fd0b467679a457f7f8093b90d40cce4
unknown <daria.lantyugova@mail.ru>
2023-09-06 01:08:45 +0300

    Table.jsx updated

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import words from '../../data/words.json'
 import edit from '../../assets/icons/edit.png';
 import del from '../../assets/icons/delete.png';
@@ -8,30 +9,19 @@ import st from './table.module.scss';
 
 
 
-class Table extends React.Component {
-  constructor(props) {
-    super(props)
-  
-    this.state = {
-       visibility : false,
-       pressed : false,
-    };
-    this.handleClick =  this.handleClick.bind(this);
-  }
+export default function Table() {
+  const [visibility, setVisibility] = useState(false);
+  const [pressed, setPressed] = useState(false);
 
-  handleClick() {
-    this.setState({
-      visibility : !this.state.visibility,
-      pressed :!this.state.pressed,
-    });
+  const handleClick = () => {
+    setVisibility(!visibility);
+    setPressed(!pressed);
   }
-  render() {
-    if (this.state.visibility) {
   return (
 <div className={st.wordlist}>
 <h1 className={st.wordlist__title} >List of words</h1>
 <div className={st.wordlist__optionButtonsContainer}>
-<button className={st.wordlist__optionButton} onClick = {this.handleClick}>{this.state.pressed ? "Study words" : "Add a new word"}</button>
+<button className={st.wordlist__optionButton} onClick = {handleClick}>{pressed ? "Study words" : "Add a new word"}</button>
 </div>
 <table className={st.wordlist__table}>
 <thead>
@@ -41,6 +31,7 @@ class Table extends React.Component {
 <th>Translation</th>
 <th>Options</th>
 </tr>
+{visibility ?
 <tr className={st.wordlist__inputRow}>
     <td><input className={st.wordlist__input} type="text" placeholder="Word"></input></td>
     <td><input className={st.wordlist__input}  type="text" placeholder="Transcription"></input></td>
@@ -48,36 +39,8 @@ class Table extends React.Component {
     <td>
     <img src={save} alt="save" className={st.wordlist__actionIcon} title="Save"/>
     <img src={cancel} alt="cancel"className={st.wordlist__actionIcon} title="Cancel"/></td>
-</tr>
-</thead>
-<tbody>{words.map((item, index) => (
-    <tr className='wordlist__item' key={index}>
-  <td>{item.english}</td>
-  <td>{item.transcription}</td>
-  <td>{item.russian}</td>
-  <td>
-    <img src={edit} alt="edit" className={st.wordlist__actionIcon} title="Edit"/>
-    <img src={del} alt="delete"className={st.wordlist__actionIcon} title="Delete"/></td>
-</tr>
-))}</tbody>
-</table>
-</div>
-);
-} else {
-  return (
-    <div className={st.wordlist}>
-<h1 className={st.wordlist__title} >List of words</h1>
-<div className={st.wordlist__optionButtonsContainer}>
-<button className={st.wordlist__optionButton} onClick = {this.handleClick}>Add a new word</button>
-</div>
-<table className={st.wordlist__table}>
-<thead>
-<tr>
-<th>Word</th>
-<th>Transcription</th>
-<th>Translation</th>
-<th>Options</th>
-</tr>
+</tr> : ''
+}
 </thead>
 <tbody>{words.map((item, index) => (
     <tr className='wordlist__item' key={index}>
@@ -93,7 +56,3 @@ class Table extends React.Component {
 </div>
   )
 }
-}
-}
-
-export default Table;

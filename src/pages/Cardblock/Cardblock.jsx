6e3e247069b3bb680bd2ac2b94fb6240158bc828
unknown <daria.lantyugova@mail.ru>
2023-09-09 22:36:40 +0300

    cardblock.jsx modified, btnRef modified in card.jsx

--- src/pages/Cardblock/Cardblock.jsx
+++ src/pages/Cardblock/Cardblock.jsx
@@ -1,19 +1,27 @@
 import React from "react";
-import Card from "../../components/Card/Card"
+import { useState } from "react";
+import Card from "../../components/Card/Card";
 import flashcards from "../../data/flashcard.json";
 import st from "./cardblock.module.scss";
 
 export default function Cardblock() {
+  const [learnt, setLearnt] = useState(0);
+
   return (
     <>
-    <div className={st.cardblock}>
-    <h1 className={st.title}>Flashcards</h1>
-    <div className={st.container}>
-      {flashcards.map((item, index) => (
-        <Card item={item} key={index}/>
-      ))}
-    </div>
-    </div>
+      <div className={st.cardblock}>
+        <h1 className={st.title}>Flashcards</h1>
+        <div className={st.container}>
+          {flashcards.map((item, index) => (
+            <Card
+              item={item}
+              key={index}
+              learnt={learnt}
+              setLearnt={setLearnt}
+            />
+          ))}
+        </div>
+      </div>
     </>
   );
 }

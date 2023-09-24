962032c9419464581dff48fc42eb4ee5290318fe
unknown <daria.lantyugova@mail.ru>
2023-09-24 14:30:12 +0300

    EmptyCard.jsx added, Slider.jsx modified

---
+++ src/components/EmptyCard/EmptyCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import st from "./emptycard.module.scss";
+
+
+export default function EmptyCard() {
+  return (
+    <div className={st.flashcard}>
+    <h2 className={st.flashcard__word}>Нет данных</h2>
+  </div>
+  )
+}
--- src/pages/Slider/Slider.jsx
+++ src/pages/Slider/Slider.jsx
@@ -1,57 +1,50 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { useContext } from 'react';
-import Context from '../../context/Context';
+import { useContext } from "react";
+import Context from "../../context/Context";
 import Card from "../../components/Card/Card";
+import EmptyCard from "../../components/EmptyCard/EmptyCard";
 import st from "./slider.module.scss";
 
 export default function Slider(props) {
-  const {wordsApi} = useContext(Context);
+  const { wordsApi } = useContext(Context);
   const [learnt, setLearnt] = useState(0);
-
- 
 
   let cardsIndexArray = []; //массив с индексами карточек
   let cardIndex; // индекс карточки
 
-
-if (wordsApi.length !== 0) {
-  for (let card of wordsApi) {
-    cardIndex = wordsApi.indexOf(card);
-    cardsIndexArray.push(cardIndex);
+  if (wordsApi.length !== 0) {
+    for (let card of wordsApi) {
+      cardIndex = wordsApi.indexOf(card);
+      cardsIndexArray.push(cardIndex);
     }
   }
-  
 
   // console.log(cardsIndexArray);
 
   // задаем состояние - начальный индекс карточки к показу 0
+
   const [selectedIndex, setSelectedIndex] = useState(cardsIndexArray[0]);
-
-
-
 
   //  функция показа следующей карточки
   const showNextCard = () => {
-    if (selectedIndex < cardsIndexArray.length-1) {
-    setSelectedIndex(selectedIndex + 1);
-    console.log(selectedIndex);
-  } else {
-    setSelectedIndex(cardsIndexArray[0]);
-  }
+    if (selectedIndex < cardsIndexArray.length - 1) {
+      setSelectedIndex(selectedIndex + 1);
+      console.log(selectedIndex);
+    } else {
+      setSelectedIndex(cardsIndexArray[0]);
+    }
   };
 
   //  функция показа предыдущей карточки
   const showPrevCard = () => {
     if (selectedIndex > cardsIndexArray[0]) {
-    setSelectedIndex(selectedIndex - 1);
-    console.log(selectedIndex);
-  } else {
-    setSelectedIndex(cardsIndexArray.length-1);
-  }
+      setSelectedIndex(selectedIndex - 1);
+      console.log(selectedIndex);
+    } else {
+      setSelectedIndex(cardsIndexArray.length - 1);
+    }
   };
-
-
 
   return (
     <>
@@ -59,24 +52,38 @@ if (wordsApi.length !== 0) {
         <h1 className={st.slider__title}>Random card learning</h1>
         <div className={st.slider}>
           <button className={st.slider__button} onClick={showPrevCard}>
-          <span className={st.slider__arrow}>&lt;</span>
-          </button> {
-          wordsApi.length !== 0 ?
-          <Card
-          item={wordsApi[selectedIndex]}
-          learnt = {learnt}
-          setLearnt={setLearnt}>
-          </Card> :
-          <div> Нет данных</div>
-        }
+            <span className={st.slider__arrow}>&lt;</span>
+          </button>{" "}
+          {wordsApi.length !== 0 ? (
+            <Card
+              item={wordsApi[selectedIndex]}
+              learnt={learnt}
+              setLearnt={setLearnt}
+            ></Card>
+          ) : (
+            <EmptyCard />
+          )}
           <button className={st.slider__button} onClick={showNextCard}>
-          <span className={st.slider__arrow}>&gt;</span>
+            <span className={st.slider__arrow}>&gt;</span>
           </button>
         </div>
-        <div className={st.slider__counter}>{selectedIndex+1} / {cardsIndexArray.length}</div>
-        <div className={st.slider__learnt}>Изучено слов: <strong>{learnt}</strong></div>
+        {wordsApi.length !== 0 ? (
+          <div className={st.slider__counter}>
+            {selectedIndex + 1} / {cardsIndexArray.length}
+          </div>
+        ) : (
+          <div className={st.slider__counter}> 0 / 0</div>
+        )}
+        {wordsApi.length !== 0 ? (
+          <div className={st.slider__learnt}>
+            Изучено слов: <strong>{learnt}</strong>
+          </div>
+        ) : (
+          <div className={st.slider__learnt}>
+            Изучено слов: <strong>0</strong>
+          </div>
+        )}
       </div>
     </>
   );
-} 
-
+}

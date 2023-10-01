94f77e081dc9ad3531a96eb9da65a865602f9654
unknown <daria.lantyugova@mail.ru>
2023-10-02 00:38:40 +0300

    CustomLink.jsx update

--- src/components/CustomLink/CustomLink.jsx
+++ src/components/CustomLink/CustomLink.jsx
@@ -4,7 +4,7 @@ import st from './customlink.module.scss';
 
 export default function Customlink({children, to, ...props}) {
     const match = useMatch(to);
-    console.log({match});
+    // console.log({match});
   return (
 <Link to={to}
 {...props} className={match ? `${st.chosen}` : `${st.notChosen}` }>
